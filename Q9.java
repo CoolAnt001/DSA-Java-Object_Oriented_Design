6a230c4af0bcbe49963dcb4d83b224190e0917cf
Amartya Bhaumik <155053095+CoolAnt001@users.noreply.github.com>
2026-07-12 19:52:37 +0530

    Update Q9.java

--- Q9.java
+++ Q9.java
@@ -7,28 +7,28 @@ class Employee {
 
     // Constructor
     Employee(String n, double s) {
-        name = n;
-        salary = s;
-    }
-
+        name = n;  
+        salary = s; 
+    } 
+ 
     // Method to display employee info
     void displayEmployeeInfo() {
         System.out.println("Employee Name: " + name);
         System.out.println("Salary: " + salary);
     }
-}
-
+} 
+ 
 class Manager extends Employee {
-
+ 
     String department;
 
     // Constructor
     Manager(String n, double s, String d) {
         super(n, s);   // calling Employee constructor
         department = d;
-    }
-
-    // Method to display manager details
+    } 
+ 
+   // Method to display manager details
     void displayManagerDetails() {
         super.displayEmployeeInfo();  // calling parent method
         System.out.println("Department: " + department);
