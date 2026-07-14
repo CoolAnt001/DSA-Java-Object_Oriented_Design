cdf8fc7a52dccc564b45962a2bb9dfb568f91a85
Amartya Bhaumik <155053095+CoolAnt001@users.noreply.github.com>
2026-07-14 20:52:29 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -10,24 +10,24 @@ class Book{
     	    this.price=price;
     	    this.quantity=quantity;
     	    totalAmount+=totalCost(price,quantity);
-    }
+    } 
     public double totalCost(double price,int quantity) {
     	    return price*quantity;
     }
     public void displayDetails() {
     	    System.out.println("Book ID:"+this.bookId+"\nPrice:"+this.price+"\nQuantity"+this.quantity);
     	    
-    }
+    } 
 	
 }
 public class Q3 {
     public static void main(String[] args) {
 
-        Book b1 = new Book(101, 500, 2);
-        Book b2 = new Book(102, 300, 1);
-        Book b3 = new Book(103, 450, 3);
-        Book b4 = new Book(104, 200, 4);
-        Book b5 = new Book(105, 150, 2);
+        Book b1 = new Book(101, 500, 3);
+        Book b2 = new Book(102, 900, 1);
+        Book b3 = new Book(103, 450, 4);
+        Book b4 = new Book(104, 250, 2);
+        Book b5 = new Book(105, 150, 7);
 
         b1.displayDetails();
         b2.displayDetails();
